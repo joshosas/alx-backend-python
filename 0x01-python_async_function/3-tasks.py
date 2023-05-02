ebbdc83d56765fb866818a5469ba36d6107c69c0
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2023-05-02 21:27:29 +0000

    pycodestyle corrections

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -11,4 +11,3 @@ def task_wait_random(max_delay: int) -> asyncio.Task:
     '''creates an async function and returns a asyncio.Task.
     '''
     return asyncio.create_task(wait_random(max_delay))
-
